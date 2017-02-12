cda9ab2b093786f89ac26fbc56e2a327c4632cbb
Mariusz Figiel <mariusz.figiel1@live.com>
2017-02-12 14:10:05 +0100

    file reading w error handling

--- matrixmap_clearcode2.py
+++ matrixmap_clearcode2.py
@@ -1,14 +1,23 @@
 lines = open('map_input.txt','r').read().split('\n')
-n = int(lines[0])
-l_cours = 0
+l_cours = 0 #init line coursor
+m_size = int(lines[l_cours]) #actual matrix size
 dict_counter = 1
 fileDict = {}
 
-values = []
-for i in range(l_cours,l_cours+n):
-    values.append(lines[i+1].split(','))
-fileDict[str(dict_counter)] = values
+while l_cours<len(lines) and isinstance(m_size, int):
+    values = []
+    for i in range(l_cours,l_cours+m_size):
+        values.append(lines[i+1].split(','))
+    fileDict[str(dict_counter)] = values
+    print(fileDict)
+    print(m_size)
 
-print(fileDict)
-
-lines.close()
+    dict_counter+=1
+    l_cours+=(m_size+1)
+    m_size = lines[l_cours]
+    try:
+        m_size = int(m_size)
+    except ValueError:
+        print("Couldn't set matrix size. Propably reached EOF.")
+        print(5*'x','stopped reading file',5*'x')
+        break
